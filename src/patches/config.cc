12c1e74076012fce0971bb45074b9a8d94b725a9
Mark Brugnoli-Vinten <netniv@hotmail.com>
2023-09-27 15:11:45 +0100

    Create default configuration file with values

--- src/patches/config.cc
+++ src/patches/config.cc
@@ -3,14 +3,38 @@
 #include <algorithm>
 #include <cstdint>
 #include <filesystem>
+#include <iostream>
 #include <string>
 #include <string_view>
-
 #include <toml++/toml.h>
 
 #include <spdlog/spdlog.h>
 
 #include <absl/strings/str_split.h>
+
+std::map<std::string, int> bannerTypes{
+    {"Standard", ToastState::Standard},
+    {"FactionWarning", ToastState::FactionWarning},
+    {"FactionLevelUp", ToastState::FactionLevelUp},
+    {"FactionLevelDown", ToastState::FactionLevelDown},
+    {"FactionDiscovered", ToastState::FactionDiscovered},
+    {"IncomingAttackFaction", ToastState::IncomingAttackFaction},
+    {"FleetBattle", ToastState::FleetBattle},
+    {"Victory", ToastState::Victory},
+    {"Defeat", ToastState::Defeat},
+    {"Event", ToastState::Tournament},
+    {"ArmadaCreated", ToastState::ArmadaCreated},
+    {"ArmadaCanceled", ToastState::ArmadaCanceled},
+    {"ArmadaIncomingAttack", ToastState::ArmadaIncomingAttack},
+    {"ArmadaBattleWon", ToastState::ArmadaBattleWon},
+    {"ArmadaBattleLost", ToastState::ArmadaBattleLost},
+    {"DiplomacyUpdated", ToastState::DiplomacyUpdated},
+    {"JoinedTakeover", ToastState::JoinedTakeover},
+    {"CompetitorJoinedTakeover", ToastState::CompetitorJoinedTakeover},
+    {"AbandonedTerritory", ToastState::AbandonedTerritory},
+    {"TakeoverVictory", ToastState::TakeoverVictory},
+    {"TakeoverDefeat", ToastState::TakeoverDefeat},
+};
 
 Config::Config()
 {
@@ -23,11 +47,67 @@ Config& Config::Get()
   return config;
 }
 
+std::string get_config_type_as_string(toml::node_type type)
+{
+  switch (type) {
+    case toml::node_type::none:
+      return "Not-a-node.";
+    case toml::node_type::table:
+      return "toml::table.";
+    case toml::node_type::array:
+      return "toml::array.";
+    case toml::node_type::string:
+      return "toml::value<std::string>.";
+    case toml::node_type::integer:
+      return "toml::value<int64_t>.";
+    case toml::node_type::floating_point:
+      return "toml::value<double>.";
+    case toml::node_type::boolean:
+      return "toml::value<bool>.";
+    case toml::node_type::date:
+      return "toml::value<date>.";
+    case toml::node_type::time:
+      return "toml::value<time>.";
+    case toml::node_type::date_time:
+      return "toml::value<date_time>.";
+  };
+
+  return "The node type is unknown";
+}
+
+template <typename T>
+inline T get_config_or_default(toml::table config, toml::table& new_config, std::string_view section, std::string_view item, T default_value)
+{
+  std::stringstream message;
+
+  message.str("");
+  message << "getting config value " << section << "." << item;
+  spdlog::info(message.str());
+
+  config.emplace<toml::table>(section, toml::table());
+  new_config.emplace<toml::table>(section, toml::table());
+
+  message.str("");
+  message << "        config value " << section << "." << item << "  type: " << get_config_type_as_string(config[section][item].type());
+  spdlog::info(message.str());
+
+  auto sectionTable = new_config[section];
+  auto final_value  = config[section][item].value_or(default_value);
+  sectionTable.as_table()->insert_or_assign(item, final_value);
+
+  message.str("");
+  message << "        config value " << section << "." << item << " value: " << final_value;
+  spdlog::info(message.str());
+
+  return (T)final_value;
+}
+
 void Config::Load()
 {
   spdlog::info("Loading Config");
 
   toml::table config;
+  toml::table parsed;
   bool        write_config = false;
   try {
     config       = toml::parse_file("community_patch_settings.toml");
@@ -40,109 +120,97 @@ void Config::Load()
     write_config = false;
   }
 
-  this->fix_unity_web_requests = config["tech"]["fix_unity_web_requests"].value_or(true);
+  this->ui_scale                    = get_config_or_default(config, parsed, "graphics", "ui_scale", 0.9f);
+  this->zoom                        = get_config_or_default(config, parsed, "graphics", "zoom", 2500.f);
+  this->free_resize                 = get_config_or_default(config, parsed, "graphics", "free_resize", true);
+  this->adjust_scale_res            = get_config_or_default(config, parsed, "graphics", "adjust_scale_res", false);
+  this->system_pan_momentum_falloff = get_config_or_default(config, parsed, "graphics", "system_pan_momentum_falloff", 0.8f);
+  this->keyboard_zoom_speed         = get_config_or_default(config, parsed, "graphics", "keyboard_zoom_speed", 350.0f);
+  this->borderless_fullscreen_f11   = get_config_or_default(config, parsed, "graphics", "borderless_fullscreen_f11", true);
+  this->target_framerate            = get_config_or_default(config, parsed, "graphics", "target_framerate", 60);
+  this->vsync                       = get_config_or_default(config, parsed, "graphics", "vsync", 1);
+  this->transition_time             = get_config_or_default(config, parsed, "graphics", "transition_time", 0.01f);
+  this->show_all_resolutions        = get_config_or_default(config, parsed, "graphics", "show_all_resolutions", false);
+  this->default_system_zoom         = get_config_or_default(config, parsed, "graphics", "default_system_zoom", 0.0f);
 
-  this->ui_scale                    = config["graphics"]["ui_scale"].value_or(0.9f);
-  this->zoom                        = config["graphics"]["zoom"].value_or(2500.f);
-  this->free_resize                 = config["graphics"]["free_resize"].value_or(true);
-  this->adjust_scale_res            = config["graphics"]["adjust_scale_res"].value_or(false);
-  this->system_pan_momentum_falloff = config["graphics"]["system_pan_momentum_falloff"].value_or(0.8f);
-  this->keyboard_zoom_speed         = config["graphics"]["keyboard_zoom_speed"].value_or(350.0f);
-  this->borderless_fullscreen_f11   = config["graphics"]["borderless_fullscreen_f11"].value_or(true);
-  this->target_framerate            = config["graphics"]["target_framerate"].value_or(60);
-  this->vsync                       = config["graphics"]["vsync"].value_or(1);
-  this->transition_time             = config["graphics"]["transition_time"].value_or(0.01f);
-  this->show_all_resolutions        = config["graphics"]["show_all_resolutions"].value_or(false);
-  this->default_system_zoom         = config["graphics"]["default_system_zoom"].value_or(0.0f);
+  this->system_zoom_preset_1 = get_config_or_default(config, parsed, "graphics", "system_zoom_preset_1", 0.0f);
+  this->system_zoom_preset_2 = get_config_or_default(config, parsed, "graphics", "system_zoom_preset_2", 0.0f);
+  this->system_zoom_preset_3 = get_config_or_default(config, parsed, "graphics", "system_zoom_preset_3", 0.0f);
+  this->system_zoom_preset_4 = get_config_or_default(config, parsed, "graphics", "system_zoom_preset_4", 0.0f);
+  this->system_zoom_preset_5 = get_config_or_default(config, parsed, "graphics", "system_zoom_preset_5", 0.0f);
 
-  this->system_zoom_preset_1 = config["graphics"]["system_zoom_preset_1"].value_or(0.0f);
-  this->system_zoom_preset_2 = config["graphics"]["system_zoom_preset_2"].value_or(0.0f);
-  this->system_zoom_preset_3 = config["graphics"]["system_zoom_preset_3"].value_or(0.0f);
-  this->system_zoom_preset_4 = config["graphics"]["system_zoom_preset_4"].value_or(0.0f);
-  this->system_zoom_preset_5 = config["graphics"]["system_zoom_preset_5"].value_or(0.0f);
+  this->hotkeys_enabled     = get_config_or_default(config, parsed, "control", "hotkeys_enabled", true);
+  this->use_scopely_hotkeys = get_config_or_default(config, parsed, "control", "use_scopely_hotkeys", false);
 
-  this->hotkeys_enabled     = config["control"]["hotkeys_enabled"].value_or(true);
-  this->use_scopely_hotkeys = config["control"]["use_scopely_hotkeys"].value_or(false);
+  this->use_out_of_dock_power = get_config_or_default(config, parsed, "buffs", "use_out_of_dock_power", false);
 
-  this->use_out_of_dock_power = config["buffs"]["use_out_of_dock_power"].value_or(false);
+  this->disable_toast_banners  = get_config_or_default(config, parsed, "ui", "disable_toast_banners", true);
+  this->extend_donation_slider = get_config_or_default(config, parsed, "ui", "extend_donation_slider", false);
+  this->disable_galaxy_chat    = get_config_or_default(config, parsed, "ui", "disable_galaxy_chat", false);
+  this->show_cargo_default     = get_config_or_default(config, parsed, "ui", "show_cargo_default", false);
+  this->show_player_cargo      = get_config_or_default(config, parsed, "ui", "show_player_cargo", false);
+  this->show_station_cargo     = get_config_or_default(config, parsed, "ui", "show_station_cargo", true);
+  this->show_hostile_cargo     = get_config_or_default(config, parsed, "ui", "show_hostile_cargo", true);
+  this->show_armada_cargo      = get_config_or_default(config, parsed, "ui", "show_armada_cargo", true);
 
-  this->disable_toast_banners  = config["ui"]["disable_toast_banners"].value_or(true);
-  this->extend_donation_slider = config["ui"]["extend_donation_slider"].value_or(false);
-  this->disable_galaxy_chat    = config["ui"]["disable_galaxy_chat"].value_or(false);
-  this->show_cargo_default     = config["ui"]["show_cargo_default"].value_or(false);
-  this->show_player_cargo      = config["ui"]["show_player_cargo"].value_or(false);
-  this->show_station_cargo     = config["ui"]["show_station_cargo"].value_or(true);
-  this->show_hostile_cargo     = config["ui"]["show_hostile_cargo"].value_or(true);
-  this->show_armada_cargo      = config["ui"]["show_armada_cargo"].value_or(true);
+  this->always_skip_reveal_sequence  = get_config_or_default(config, parsed, "ui", "always_skip_reveal_sequence", false);
+  this->stay_in_bundle_after_summary = get_config_or_default(config, parsed, "ui", "stay_in_bundle_after_summary", true);
 
-  this->always_skip_reveal_sequence  = config["ui"]["always_skip_reveal_sequence"].value_or(false);
-  this->stay_in_bundle_after_summary = config["ui"]["stay_in_bundle_after_summary"].value_or(true);
+  this->fix_unity_web_requests = get_config_or_default(config, parsed, "tech", "fix_unity_web_requests", true);
 
-  std::string disabled_banner_types_str = config["ui"]["disabled_banner_types"].value_or("");
+  // must explicitly included std::string typing here or we get back char * which fails us!
+  std::string disabled_banner_types_str = get_config_or_default<std::string>(config, parsed, "ui", "disabled_banner_types", "");
 
   std::vector<std::string> types = absl::StrSplit(disabled_banner_types_str, ",", absl::SkipWhitespace());
-  for (const absl::string_view _type : types) {
-    auto type = absl::StripLeadingAsciiWhitespace(_type);
-    if (type == "Standard") {
-      this->disabled_banner_types.emplace_back(ToastState::Standard);
-    } else if (type == "FactionWarning") {
-      this->disabled_banner_types.emplace_back(ToastState::FactionWarning);
-    } else if (type == "FactionLevelUp") {
-      this->disabled_banner_types.emplace_back(ToastState::FactionLevelUp);
-    } else if (type == "FactionLevelDown") {
-      this->disabled_banner_types.emplace_back(ToastState::FactionLevelDown);
-    } else if (type == "FactionDiscovered") {
-      this->disabled_banner_types.emplace_back(ToastState::FactionDiscovered);
-    }
-    /* else if (type == "IncomingAttack") {
-        this->disabled_banner_types.emplace_back(ToastState::IncomingAttack);
-    } */
-    else if (type == "IncomingAttackFaction") {
-      this->disabled_banner_types.emplace_back(ToastState::IncomingAttackFaction);
-    } else if (type == "FleetBattle") {
-      this->disabled_banner_types.emplace_back(ToastState::FleetBattle);
-    } /*else if (type == "StationBattle") {
-        this->disabled_banner_types.emplace_back(ToastState::StationBattle);
-    } else if (type == "StationVictory") {
-        this->disabled_banner_types.emplace_back(ToastState::StationVictory);
-    } else if (type == "StationDefeat") {
-        this->disabled_banner_types.emplace_back(ToastState::StationDefeat);
-    } */
-    else if (type == "Victory") {
-      this->disabled_banner_types.emplace_back(ToastState::Victory);
-    } else if (type == "Defeat") {
-      this->disabled_banner_types.emplace_back(ToastState::Defeat);
-    } else if (type == "Event") {
-      this->disabled_banner_types.emplace_back(ToastState::Tournament);
-    } else if (type == "ArmadaCreated") {
-      this->disabled_banner_types.emplace_back(ToastState::ArmadaCreated);
-    } else if (type == "ArmadaCanceled") {
-      this->disabled_banner_types.emplace_back(ToastState::ArmadaCanceled);
-    } else if (type == "ArmadaIncomingAttack") {
-      this->disabled_banner_types.emplace_back(ToastState::ArmadaIncomingAttack);
-    } else if (type == "ArmadaBattleWon") {
-      this->disabled_banner_types.emplace_back(ToastState::ArmadaBattleWon);
-    } else if (type == "ArmadaBattleLost") {
-      this->disabled_banner_types.emplace_back(ToastState::ArmadaBattleLost);
-    } else if (type == "DiplomacyUpdated") {
-      this->disabled_banner_types.emplace_back(ToastState::DiplomacyUpdated);
-    } else if (type == "JoinedTakeover") {
-      this->disabled_banner_types.emplace_back(ToastState::JoinedTakeover);
-    } else if (type == "CompetitorJoinedTakeover") {
-      this->disabled_banner_types.emplace_back(ToastState::CompetitorJoinedTakeover);
-    } else if (type == "AbandonedTerritory") {
-      this->disabled_banner_types.emplace_back(ToastState::AbandonedTerritory);
-    } else if (type == "TakeoverVictory") {
-      this->disabled_banner_types.emplace_back(ToastState::TakeoverVictory);
-    } else if (type == "TakeoverDefeat") {
-      this->disabled_banner_types.emplace_back(ToastState::TakeoverDefeat);
+
+  std::string       bannerString = "";
+  std::stringstream message;
+  message << "Parsing banner strings";
+
+  spdlog::info(message.str());
+
+  for (const auto& [key, value] : bannerTypes) {
+    auto lower_key = absl::AsciiStrToLower(key);
+
+    for (const std::string_view _type : types) {
+      auto stripped_type = absl::StripLeadingAsciiWhitespace(_type);
+      auto lower_type = absl::AsciiStrToLower(stripped_type);
+
+      if (lower_key == lower_type) {
+        this->disabled_banner_types.emplace_back(value);
+        if (bannerString.length() != 0) {
+          bannerString.append(", ");
+        }
+        bannerString.append(key);
+      }
     }
   }
 
+  message.str("");
+  message << "Final disabledbanner types: " << bannerString;
+  spdlog::info(message.str());
+
+  parsed["ui"].as_table()->insert_or_assign("disabled_banner_types", bannerString);
+
   if (!std::filesystem::exists("community_patch_settings.toml")) {
+    message.str("");
+    message << "Creating new config file";
+    spdlog::warn(message.str());
+
     std::ofstream config_file;
     config_file.open("community_patch_settings.toml");
-    config_file << config;
+    config_file << parsed;
     config_file.close();
   }
+
+  message.str("");
+  message << "Creating paresd config file";
+  spdlog::info(message.str());
+
+  std::ofstream config_file;
+  config_file.open("community_patch_settings_parsed.toml");
+  config_file << parsed;
+  config_file.close();
+
+  std::cout << "Running config: \n" << parsed << "\n\n";
 }
