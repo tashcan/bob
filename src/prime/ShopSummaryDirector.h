56300cffbb0414021c0c3101882b8dc53952840c
Tashcan <git@tashcan.com>
2023-09-29 14:52:19 -0700

    Restrict the stay in bundle behavior to gacha and events

--- src/prime/ShopSummaryDirector.h
+++ src/prime/ShopSummaryDirector.h
@@ -10,12 +10,12 @@ class ShopSummaryDirector
 {
 public:
   __declspec(property(get = __get__backLogicSkipSectionIds)) Il2CppArraySize* _backLogicSkipSectionIds;
+  __declspec(property(get = __get_backlogicCache)) Il2CppArraySize* backlogicCache;
 
 private:
   static IL2CppClassHelper& get_class_helper()
   {
-    static auto class_helper =
-        il2cpp_get_class_helper("Assembly-CSharp", "Digit.Prime.Shop", "ShopSummaryDirector");
+    static auto class_helper = il2cpp_get_class_helper("Assembly-CSharp", "Digit.Prime.Shop", "ShopSummaryDirector");
     return class_helper;
   }
 
@@ -23,6 +23,12 @@ public:
   Il2CppArraySize* __get__backLogicSkipSectionIds()
   {
     static auto field = get_class_helper().GetField(xorstr_("_backLogicSkipSectionIds")).offset();
+    return *(Il2CppArraySize**)((char*)this + field);
+  }
+
+  Il2CppArraySize* __get_backlogicCache()
+  {
+    static auto field = get_class_helper().GetField(xorstr_("backlogicCache")).offset();
     return *(Il2CppArraySize**)((char*)this + field);
   }
 };
