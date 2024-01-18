0526f14f20477cf3d2fde8b67a8a60dba6a1c20b
Mark Brugnoli-Vinten <netniv@hotmail.com>
2024-01-18 02:16:41 +0000

    Update beta version number

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               5
 #define VERSION_REVISION            3
-#define VERSION_PATCH               4
+#define VERSION_PATCH               5
 
 #define STRINGIFY_(s)               #s
 #define STRINGIFY(s)                STRINGIFY_(s)
