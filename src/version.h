579eb4165e08419cf22035703bac3878db2de304
Mark Brugnoli-Vinten <netniv@hotmail.com>
2024-03-22 23:19:10 +0000

    Make 0.7 release

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               6
 #define VERSION_REVISION            0
-#define VERSION_PATCH               8
+#define VERSION_PATCH               0
 
 #define STRINGIFY_(s)               #s
 #define STRINGIFY(s)                STRINGIFY_(s)
