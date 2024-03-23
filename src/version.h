0b72511454f729a4badb14a172af77c9624fe72d
Mark Brugnoli-Vinten <netniv@hotmail.com>
2024-03-23 13:57:10 +0000

    Update dev version to v0.6.1 Patch 1

--- src/version.h
+++ src/version.h
@@ -3,8 +3,8 @@
 // clang-format off
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               6
-#define VERSION_REVISION            0
-#define VERSION_PATCH               0
+#define VERSION_REVISION            1
+#define VERSION_PATCH               1
 
 #define STRINGIFY_(s)               #s
 #define STRINGIFY(s)                STRINGIFY_(s)
