b806b23d38efa8a1a79867445e25c8efa2c7cf4a
Mark Brugnoli-Vinten <netniv@hotmail.com>
2024-04-10 14:36:51 +0100

    Version bump

--- mods/src/version.h
+++ mods/src/version.h
@@ -4,7 +4,7 @@
 #define VERSION_MAJOR               0
 #define VERSION_MINOR               6
 #define VERSION_REVISION            1
-#define VERSION_PATCH               3
+#define VERSION_PATCH               4
 
 #define STRINGIFY_(s)               #s
 #define STRINGIFY(s)                STRINGIFY_(s)
