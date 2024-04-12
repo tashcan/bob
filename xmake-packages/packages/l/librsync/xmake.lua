65b8dfd1bb07274f3e740fdff7025c25261a6ec5
Tashcan <git@tashcan.com>
2024-04-11 19:07:21 -0700

    Remove hardcoded OSX architechtures from xmake packages

--- xmake-packages/packages/l/librsync/xmake.lua
+++ xmake-packages/packages/l/librsync/xmake.lua
@@ -8,6 +8,5 @@ on_install(function(package)
     local configs = {}
     table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
     table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
-    table.insert(configs, "-DCMAKEY_SYSTEM_PROCESSOR=x86_64")
     import("package.tools.cmake").install(package, configs)
 end)
